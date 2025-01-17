9f1940783cd568116421fd08e277bb6a00804848
Frentoni  Eric <eric.frentoni@porsche-engineering.ro>
2025-01-17 15:39:53 +0200

    Add new features

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from PIL import Image
 import os
 
+print("started>>>>>>>>>>>>>>")
+repo_path = os.getcwd()
 def convert(inPath, outPath):
-    with Image.open(inPath) as img:
-        img.save(outPath, "PNG")
+    for root, dirs, files in os.walk(inPath):
+        for file in files:
+            file_path = inPath = os.path.join(root, file)
+            img = Image.open(file_path)
+            output_path = os.path.join(root, os.path.splitext(file)[0] + ".png")
+            img.save(output_path, "PNG")
+            print("image saved")
 
 
-inPath = "img1.jpg"
-outPath = "img1.png"
+inPath = os.path.join(repo_path, 'data') 
+outPath = os.path.join(repo_path, 'data')
 
 convert(inPath, outPath)
